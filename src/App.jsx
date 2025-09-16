3f19a130ecf1ae11cff7c932ff995165b6ac1fad
bezalel6 <bezalel3250@gmail.com>
2025-09-16 07:54:02 +0300

    Add header component with logo and GitHub link

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import InviteCard from './components/InviteCard'
+import Header from './components/Header'
 import './App.css'
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<InviteCard />} />
-        <Route path="/invite/:id" element={<InviteCard />} />
-      </Routes>
+      <Header />
+      <div style={{ paddingTop: '60px' }}>
+        <Routes>
+          <Route path="/" element={<InviteCard />} />
+          <Route path="/invite/:id" element={<InviteCard />} />
+        </Routes>
+      </div>
     </Router>
   )
 }
